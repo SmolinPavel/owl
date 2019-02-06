7ef32993e08c4cbd166c034dc97e75e9c9162508
Pavel Smolin <isingwithaz@gmail.com>
2019-02-07 02:24:17 +0300

    Add owl

--- pages/owl.js
+++ pages/owl.js
@@ -1,7 +1,52 @@
 import Layout from "../components/MyLayout.js";
 
+import "./owl.css";
+
 export default () => (
   <Layout>
-    <p>This is the owl page</p>
+    <h1>The Owl is watching you!</h1>
+    <div className="box">
+      <div className="body">
+        <div className="head" />
+
+        <div className={["ear", "left"].join(" ")}>
+          <div className="ear-hole" />
+        </div>
+
+        <div className={["ear", "right"].join(" ")}>
+          <div className="ear-hole" />
+        </div>
+
+        <div className={["eyelid", "left"].join(" ")} />
+        <div className="eye-left">
+          <div className="pupil">
+            <div className="dot" />
+          </div>
+        </div>
+
+        <div className={["eyelid", "right"].join(" ")} />
+        <div className="eye-right">
+          <div className="pupil">
+            <div className="dot" />
+          </div>
+        </div>
+
+        <div className="nose" />
+
+        <div className="body-copy" />
+        <div className="wing-left" />
+        <div className="wing-right" />
+        <div className="body-inner" />
+
+        <div className={["leg", "left"].join(" ")} />
+        <div className={["leg", "left-1"].join(" ")} />
+        <div className={["leg", "left-2"].join(" ")} />
+        <div className={["leg", "right"].join(" ")} />
+        <div className={["leg", "right-1"].join(" ")} />
+        <div className={["leg", "right-2"].join(" ")} />
+
+        <div id="heart" />
+      </div>
+    </div>
   </Layout>
 );
