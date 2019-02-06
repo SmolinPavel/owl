6e3e88fc49090763451331f3b7edbc23294251b8
Pavel Smolin <isingwithaz@gmail.com>
2019-02-06 23:16:27 +0300

    Add ssr support for clean urls

---
+++ server.mjs
@@ -0,0 +1,34 @@
+import express from "express";
+import next from "next";
+
+const { env, exit } = process;
+
+const dev = env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get("/p/:id", (req, res) => {
+      const { id } = req.params;
+      const actualPage = "/post";
+      const queryParams = { title: id };
+      app.render(req, res, actualPage, queryParams);
+    });
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(3000, err => {
+      if (err) throw err;
+      console.log("> Ready on http://localhost:3000");
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    exit(1);
+  });
